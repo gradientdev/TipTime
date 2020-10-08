f6a7cb771af3c262fde63ca8044cc526f2d90ff8
Xinyun Zhou <72347584+gradientdev@users.noreply.github.com>
2020-10-08 08:55:14 -0400

    Update MainActivity.kt
    Organized code a bit, added comments

--- app/src/main/java/com/gradient/tiptime/MainActivity.kt
+++ app/src/main/java/com/gradient/tiptime/MainActivity.kt
@@ -1,5 +1,5 @@
+//imports
 package com.gradient.tiptime
-
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.NumberFormat
 
+//main activity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         calculateButton.setOnClickListener{ calculateTip() }
     }
 
+    //calculates tip and other values. Is called when calculate button is tapped
     @SuppressLint("StringFormatMatches")
     fun calculateTip() {
         var mealCost: Double = cost_of_service.text.toString().toDouble()
@@ -26,26 +28,31 @@ class MainActivity : AppCompatActivity() {
         val tipPercentage = when(selectedId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
-        }
+            else -> 0.15}
 
+        //some values
         var splitnum = 0.0
         var tip = tipPercentage * mealCost
+        val addedCost = tip + mealCost
+
+        //finds the views
         val switch: Switch = findViewById(R.id.round_up_switch)
         val result: TextView = findViewById(R.id.tip_result)
         val total: TextView = findViewById(R.id.total_cost)
+        val roundUp = switch.isChecked
 //        val split: TextView = findViewById(R.id.split_cost)
 //        val splitDouble: Double = split.toString().toDouble()
 //        val splitVal: Double = (splitnum / splitDouble)
-        val roundUp = switch.isChecked
 
+        //if roundup switch is checked, rounds tip up
         if (roundUp) {
             tip = kotlin.math.ceil(tip)}
 
-        val addedCost = tip + mealCost
+        //formats the tip to $
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         val formattedCost = NumberFormat.getCurrencyInstance().format(addedCost)
 
+        //changes the textViews in the app
         result.text = getString(R.string.tip_amount, formattedTip)
         total.text = getString(R.string.total_amount, formattedCost)
 //        split.text = getString(R.string.split_cost, splitVal)
