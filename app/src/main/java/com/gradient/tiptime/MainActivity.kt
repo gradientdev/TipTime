a5ce900490a3d03d5f24f1c602873a6794a10a11
Xinyun Zhou <72347584+gradientdev@users.noreply.github.com>
2020-10-08 11:54:40 -0400

    Update MainActivity.kt
    Finished code to find the split price of a meal

--- app/src/main/java/com/gradient/tiptime/MainActivity.kt
+++ app/src/main/java/com/gradient/tiptime/MainActivity.kt
@@ -1,6 +1,5 @@
 //imports
 package com.gradient.tiptime
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
@@ -20,10 +19,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     //calculates tip and other values. Is called when calculate button is tapped
-    @SuppressLint("StringFormatMatches")
     fun calculateTip() {
         var mealCost: Double = cost_of_service.text.toString().toDouble()
-        mealCost += 0
+        var splitNum: Double = split_num.text.toString().toDouble()
+
         val selectedId = tip_options.checkedRadioButtonId
         val tipPercentage = when(selectedId) {
             R.id.option_twenty_percent -> 0.20
@@ -31,7 +30,6 @@ class MainActivity : AppCompatActivity() {
             else -> 0.15}
 
         //some values
-        var splitnum = 0.0
         var tip = tipPercentage * mealCost
         val addedCost = tip + mealCost
 
@@ -39,10 +37,11 @@ class MainActivity : AppCompatActivity() {
         val switch: Switch = findViewById(R.id.round_up_switch)
         val result: TextView = findViewById(R.id.tip_result)
         val total: TextView = findViewById(R.id.total_cost)
+        val splitResult: TextView = findViewById(R.id.split_cost)
         val roundUp = switch.isChecked
-//        val split: TextView = findViewById(R.id.split_cost)
-//        val splitDouble: Double = split.toString().toDouble()
-//        val splitVal: Double = (splitnum / splitDouble)
+
+        var split: Double = mealCost / splitNum
+        var splitString: String = split.toString()
 
         //if roundup switch is checked, rounds tip up
         if (roundUp) {
@@ -51,10 +50,11 @@ class MainActivity : AppCompatActivity() {
         //formats the tip to $
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         val formattedCost = NumberFormat.getCurrencyInstance().format(addedCost)
+        val formattedSplit = NumberFormat.getCurrencyInstance().format(split)
 
         //changes the textViews in the app
         result.text = getString(R.string.tip_amount, formattedTip)
         total.text = getString(R.string.total_amount, formattedCost)
-//        split.text = getString(R.string.split_cost, splitVal)
+        splitResult.text = getString(R.string.split_cost, formattedSplit)
     }
 }
